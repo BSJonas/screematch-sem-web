cb85d9173868b67e10e2f71fac538a395b5edafc
jonas.sousa <jonas.sousa@softplan.com.br>
2024-01-16 11:30:22 -0300

    Refinamento do projeto spring

--- src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
+++ src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
@@ -1,6 +1,5 @@
 package br.com.alura.screenmatch.service;
 
-import br.com.alura.screenmatch.model.DadosSerie;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -8,7 +7,7 @@ public class ConverteDados implements IConverteDados {
     private ObjectMapper mapper = new ObjectMapper();
 
     @Override
-    public <T> T obertDados(String json, Class<T> classe) {
+    public <T> T obterDados(String json, Class<T> classe) {
         try {
             return mapper.readValue(json, classe);
         } catch (JsonProcessingException e) {
