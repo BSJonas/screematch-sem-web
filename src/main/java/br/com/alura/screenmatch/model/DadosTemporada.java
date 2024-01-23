b9eb1b1887fe6f6d61d0d3092ef6bac349080933
jonas.sousa <jonas.sousa@softplan.com.br>
2024-01-23 08:54:02 -0300

    Explorando fluxos e gerando estatísticas

--- src/main/java/br/com/alura/screenmatch/model/DadosTemporada.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosTemporada.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.List;
-@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonIgnoreProperties(ignoreUnknown = true) //ignora demais propriedades que não estão mapeadas, neste caso por exemplo seria ano de lançamento (Yaer).
 public record DadosTemporada(@JsonAlias("Season") Integer numero,
                              @JsonAlias("Episodes") List<DadosEpisodio> episodios) {
 }
